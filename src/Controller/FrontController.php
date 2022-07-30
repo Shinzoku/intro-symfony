f661ad42da89d9ddda812568cb2fb10dfe1965ee
shinzoku <shinzoku62800@gmail.com>
2022-07-30 19:58:41 +0200

    Modif articles display in homepage with paginator
    Display 10 elements / page

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -7,13 +7,21 @@ use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; // Nous avons besoin d'accéder à la requête pour obtenir le numéro de page
+use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
 
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'app_front_index')]
-    public function index(ArticleRepository $repository): Response
+    public function index(ArticleRepository $repository, Request $request, PaginatorInterface $paginator): Response
     {
-        $articles = $repository->findNLast(5);
+        $datas = $repository->findAll();
+
+        $articles = $paginator->paginate(
+            $datas, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            20 // Nombre de résultats par page
+        );
 
         return $this->render('front/index.html.twig', [
             'articles' => $articles,
